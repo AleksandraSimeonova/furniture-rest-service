6967b3d620db8a80adb3f5ce9db579cda9b8d2ee
AleksandraSimeonova <simeonova.aleksandra9@gmail.com>
2025-06-28 18:17:51 +0300

    parse json data send from client

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,8 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json())
+
  app.use((req,res,next) => {
      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
  

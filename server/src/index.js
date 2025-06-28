f8d7f36cdf2f445c22256e1aa9f2894f7c9e88bd
AleksandraSimeonova <simeonova.aleksandra9@gmail.com>
2025-06-28 18:11:47 +0300

    cors middleware

---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+
+const app = express();
+
+ app.use((req,res,next) => {
+     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+ 
+     next()
+ })
+ 
+
+app.get('/data/catalog', (req,res)=> {
+    res.json({});
+});
+
+
+app.listen(3030, ()=>{console.log('Server is listening on port http://localhost:3030 ...')})
